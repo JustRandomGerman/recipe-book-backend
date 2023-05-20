50f33a7f56d9bb4652524dc7eb855682ddfad479
JustRandomGerman <dresdani2002@gmail.com>
2023-05-20 14:18:45 +0200

    imp. tag filtering, ingredient search, validation

--- src/controller/SearchController.ts
+++ src/controller/SearchController.ts
@@ -2,17 +2,27 @@ import { AppDataSource } from '../data-source';
 import { NextFunction, Request, Response, response } from "express";
 import { Recipe } from '../entity/Recipe';
 import { ImagePath } from '../entity/ImagePath';
+import * as Joi from 'joi';
 
 export class SearchController {
 
     private recipeRepository = AppDataSource.getRepository(Recipe);
     private baseImagePath = "http://localhost:3000/images/";
 
+    private searchSchema = Joi.object({
+        query: Joi.string().required(),
+        mode: Joi.string().lowercase().valid("recipe", "ingredient").required(),
+        tags: Joi.array().items(Joi.string())//TODO ? add check for all available ingredients
+    })
+
     async search(request: Request, response: Response, next: NextFunction) {
         const { query, mode, tags} = request.body;
 
-        //TODO validate parameters
-        //response.status(400).json({message: "incorrect parameters"})
+        const { error } = this.searchSchema.validate(request.body);
+        if (error) {
+            response.status(400).json({ message: error.details });
+            return;
+        }
 
         if(mode === 'recipe'){
             return this.searchRecipe(query, tags, response);
@@ -28,7 +38,7 @@ export class SearchController {
     async searchRecipe(query: string, tags: string[], response: Response){
         const recipes = await this.recipeRepository
         .createQueryBuilder("recipe")
-        //join all the addtional columns to the recipe
+        //join all additional columns to the recipe
         .leftJoinAndSelect("recipe.image_paths", "image_path")
         .leftJoinAndSelect("recipe.keywords", "keyword")
         .leftJoinAndSelect("recipe.ingredients", "ingredient")
@@ -38,22 +48,93 @@ export class SearchController {
         //search for recipes with matching keywords
         .orWhere("keyword.keyword_name LIKE :keyword", { keyword: `%${query}%` })
         .getMany();
+
+        //if there are filter tags only get recipes with those tags
+        if (tags.length > 0) {
+            const filteredRecipes = recipes.filter((recipe) => {
+                return tags.every((tag) => recipe.tags.some((recipeTag) => recipeTag.tag_name === tag));
+            });
         
-        if(recipes.length === 0){
+            if (filteredRecipes.length === 0) {
+                response.status(404).json({message: "No recipes could be found using your search criteria. Try a different query or remove some filter tags"});
+                return;
+            }
+        
+            filteredRecipes.forEach((recipe) => {
+                recipe.image_paths.forEach( (imagePath: ImagePath) =>
+                    (imagePath.path = this.baseImagePath + imagePath.path)
+                );
+            });
+        
+            return filteredRecipes;
+        }
+
+        if (recipes.length === 0) {
             response.status(404).json({message: "No recipes could be found using your search criteria. Try a different query or remove some filter tags"});
             return;
         }
-
-        //finish all the paths to images on the server
-        recipes.map((recipe: Recipe) => {
-            recipe.image_paths.map((imagePath : ImagePath) => imagePath.path = this.baseImagePath + imagePath.path);
+        
+        recipes.forEach((recipe: Recipe) => {
+            recipe.image_paths.forEach( (imagePath: ImagePath) =>
+                (imagePath.path = this.baseImagePath + imagePath.path)
+            );
         });
 
         return recipes;
     }
 
     async searchIngredient(query: string, tags: string[], response: Response){
+        const queryBuilder = await this.recipeRepository
+        .createQueryBuilder("recipe")
+        //join all additional columns to the recipe
+        .leftJoinAndSelect("recipe.image_paths", "image_path")
+        .leftJoinAndSelect("recipe.keywords", "keyword")
+        .leftJoinAndSelect("recipe.ingredients", "ingredient")
+        .leftJoinAndSelect("recipe.tags", "tag")
+        //search for recipes containing an ingredient
+        .where("ingredient.ingredient_name LIKE :ingredient", { ingredient: `%${query}%` });
 
+        //if there are filter tags only get recipes with those tags
+        if (tags.length > 0) {
+            tags.forEach((tag) => {
+                queryBuilder.andWhere("EXISTS (SELECT 1 FROM tag t WHERE t.recipeId = recipe.id AND t.tag_name = :tag)", { tag });
+            });
+        }
+
+        const recipes = await queryBuilder.getMany();
+
+        //if there are filter tags only get recipes with those tags
+        /*if (tags.length > 0) {
+            const filteredRecipes = recipes.filter((recipe) => {
+                return tags.every((tag) => recipe.tags.some((recipeTag) => recipeTag.tag_name === tag));
+            });
+        
+            if (filteredRecipes.length === 0) {
+                response.status(404).json({message: "No recipes could be found using your search criteria. Try a different query or remove some filter tags"});
+                return;
+            }
+        
+            filteredRecipes.forEach((recipe) => {
+                recipe.image_paths.forEach( (imagePath: ImagePath) =>
+                    (imagePath.path = this.baseImagePath + imagePath.path)
+                );
+            });
+        
+            return filteredRecipes;
+        }*/
+
+        if (recipes.length === 0) {
+            response.status(404).json({message: "No recipes could be found using your search criteria. Try a different query or remove some filter tags"});
+            return;
+        }
+        
+        recipes.forEach((recipe: Recipe) => {
+            recipe.image_paths.forEach( (imagePath: ImagePath) =>
+                (imagePath.path = this.baseImagePath + imagePath.path)
+            );
+        });
+
+        return recipes;
     }
 
 }
